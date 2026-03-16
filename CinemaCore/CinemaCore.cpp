87b94503c70c557c7a676d698f5863681e9f9045
TVDespotov23 <TVDespotov23@codingburgas.bg>
2026-03-16 14:48:43 +0200

    Add 2 static librarys

--- CinemaCore/CinemaCore.cpp
+++ CinemaCore/CinemaCore.cpp
@@ -1,10 +1,10 @@
-// Seating.cpp : Defines the functions for the static library.
+// CinemaCore.cpp : Defines the functions for the static library.
 //
 
 #include "pch.h"
 #include "framework.h"
 
 // TODO: This is an example of a library function
-void fnSeating()
+void fnCinemaCore()
 {
 }
